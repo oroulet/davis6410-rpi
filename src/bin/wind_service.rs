4ecb24e6218570fefddeab6d7fc73a34b1b875e6
Olivier <olivier@helitech>
2024-03-09 21:29:53 +0100

    change crate name

--- src/bin/wind_service.rs
+++ src/bin/wind_service.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use anyhow::Result;
 
 use clap::{arg, Command};
-use rust_wind::{api::WindServer, davis::Davis};
+use davis_rpi::{api::WindServer, davis::Davis};
 use tokio::signal;
 
 #[tokio::main]
