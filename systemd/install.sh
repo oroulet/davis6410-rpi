1ba7ac5532aca604e243393c49ba15da4e4f9ef8
olivier <olivier@rpi>
2024-06-23 19:03:28 +0200

    fixes

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,8 +1,7 @@
 set -x 
 sudo cp wind.service /etc/systemd/system/wind.service
 sudo mkdir -p /opt/public
-sudo cp ../public/index.html /opt/index.html
-sudo cp -r pu.service /etc/systemd/system/wind.service
+sudo cp ../public/* /opt/public/
 sudo systemctl daemon-reload
 sudo systemctl enable wind.service
 sudo systemctl start wind.service
